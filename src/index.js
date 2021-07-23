f4c55195bf6a6ef8f21adbef740e22b2041df86e
John M. Stassen <john.stassen@gmail.com>
2021-07-23 15:13:54 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // listen for submit
+  //   squelch default
+  //   take input and add it to My Todos
+  
+  document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let newTask = document.createElement('li');
+    newTask.innerHTML = document.getElementById("new-task-description").value;
+    let taskList = document.getElementById("tasks");
+    taskList.appendChild(newTask);
+    document.getElementById("new-task-description").value = ""
+    }, false);
+  
+
 });
